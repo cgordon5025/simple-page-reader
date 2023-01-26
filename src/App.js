7ffd2b2c0ec1e00f0fa6621dd859befe5045d3d1
Charlee Gordon <cgordon5025@gmail.com>
2023-01-26 12:13:44 -0500

    something

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
-// import { TextToSpeech } from 'text-to-speech-js';
 import React, { useCallback } from 'react';
 // keepers
 // import { useTts } from 'tts-react';
-// import Webcam from 'react-webcam';
+import Webcam from 'react-webcam';
+// import {Text}
 
-// const WebcamComponenet = () => <Webcam />
-import { useRecordWebcam } from 'react-record-webcam';
-
+// import { useRecordWebcam } from 'react-record-webcam';
+import { RecordWebcam, useRecordWebcam } from 'react-record-webcam';
 // old tried and didn't work
-// import Say from 'react-say';
+import Say, { SayButton } from 'react-say';
 
+// const OPTIONS = {
+//   fileName: `participant_1`,
+//   mimeType: "video/mp4",
+//   width: 1920,
+//   height: 1080, \
+
+// } as const;
 // import { TextToSpeech } from 'tts-react';
 // import type { TTSHookProps } from 'tts-react';
 
@@ -26,25 +32,97 @@ import { useRecordWebcam } from 'react-record-webcam';
 // }
 
 function App() {
+  const synth = window.speechSynthesis;
+  // console.log(synth.getVoices())
+  const voices = synth.getVoices().sort(function (a, b) {
+    const aname = a.name.toUpperCase();
+    const bname = b.name.toUpperCase();
+
+    if (aname < bname) {
+      return -1;
+    } else if (aname == bname) {
+      return 0;
+    } else {
+      return +1;
+    }
+  });
+  const phrase = "The quick brown fox jumped over the lazy dogs."
+  const tempPhrase = phrase.split(' ')
+  console.log(tempPhrase)
+  var spanPhrase = document.createElement('p')
+
+  for (let i = 0; i < tempPhrase.length; i++) {
+    console.log("running the span section")
+    var spanObj = document.createElement('span')
+    spanObj.textContent = (tempPhrase[i] + " ")
+    // spanPhrase.className = "spanPhrase"
+    spanPhrase.appendChild(spanObj)
+    spanPhrase.append(spanObj);
+  }
+  // console.log(spanPhrase)
+  document.getElementById('highlightContainer').appendChild(spanPhrase)
+
+  const rate = 1;
+  const daniel = voices[14];
+  const utterThis = new SpeechSynthesisUtterance(phrase)
+  utterThis.rate = rate
+  utterThis.voice = daniel
+  // var wordIndex = 0
+  //trying to get voice word tracking
+  // const words = document.getElementById('highlightContainer')
+  const words = document.getElementById('highlightContainer').firstChild
+  console.log(words)
+  const color = () => {
+    console.log("doing the highlight")
+    for (let i = 0; i < parseWords.length; i++) {
+      
+      console.log("in the loop")
+      console.log(parseWords[i])
+      words[i].style.backgroundcolor = "blue"
+    }
+  }
+  //speak it
+  const speak = () => {
+    synth.speak(utterThis)
+    color()
+  }
+  const WebcamComponenet = () => <Webcam />
+
   const recordWebcam = useRecordWebcam({ frameRate: 60 })
 
   const saveFile = async () => {
     const blob = await recordWebcam.getRecording();
   }
-  // console.log(TextToSpeech.talk("hello"))
-  // TextToSpeech.talk("Hello World")
-  // const selector = useCallback(voices => [...voices].find(v => v.lang === 'en-GB'), [])
+  const selector = useCallback(voices => [...voices].find(v => v.lang === 'en-GB'), [])
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        {/* <Say text="A quick brown fox jumped over the lazy dogs."
+        {/* <p> Hello {voices[14]}</p> */}
+        <div id="highlightContainer">
+
+        </div>
+        {/* <p id='highlight'> {spanPhrase}</p> */}
+        <p> {daniel.name} {daniel.lang}</p>
+        <button onClick={speak}> Speak as daniel</button>
+        <div>
+          {/* <Say text="A quick brown fox jumped over the lazy dogs."
           voice={selector} /> */}
-        {/* <TextToSpeech
+          {/* <SayButton
+          onClick={event => console.log(event)}
+          pitch={1.2}
+          text="A quick brown fox jumped over the lazy dogs."
+          voice={selector}> Say Something</SayButton>
+        <TextToSpeech
           markTextAsSpoken
-          lang='en-GB'>
-          <p> Hello World</p>
+          lang='en-GB'
+          rate={".9"}>
+          <p>The quick brown fox jumped over the lazy dogs</p>
         </TextToSpeech> */}
+        </div>
+        {/* with languages unable to change the pitch of the voice */}
+        {/* be sure to not have a space at the beginning because it will mess with the highlighting  */}
+
         {/* <Webcam className='d-none' /> */}
         <div>
           <p>Camera status: {recordWebcam.status}</p>
@@ -54,9 +132,10 @@ function App() {
           <button onClick={recordWebcam.retake}>Retake recording</button>
           <button onClick={recordWebcam.download}>Download recording</button>
           <button onClick={saveFile}>Save file to server</button>
-          <video style={{ display: "none" }} ref={recordWebcam.webcamRef} autoPlay muted />
-          <video style={{ display: "none" }} ref={recordWebcam.previewRef} autoPlay muted loop />
+          <video ref={recordWebcam.webcamRef} autoPlay muted />
+          <video ref={recordWebcam.previewRef} autoPlay muted loop />
         </div>
+        <recordWebcam />
 
         <p>
           Edit <code>src/App.js</code> and save to reload.
